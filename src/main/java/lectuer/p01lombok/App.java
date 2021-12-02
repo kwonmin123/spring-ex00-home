33531eef824cb4a16cc02df408981de61cb525a5
kwonmin123123 <zzacjk@naver.com>
2021-12-02 15:57:08 +0900

    Create App.java

---
+++ src/main/java/lectuer/p01lombok/App.java
@@ -0,0 +1,14 @@
+package lectuer.p01lombok;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Customer c1 = new Customer();
+
+
+c1.setAddress(address);
+c1.setAddress(address);
+	}
+
+}
