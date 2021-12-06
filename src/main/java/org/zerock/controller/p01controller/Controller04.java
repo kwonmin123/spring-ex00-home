2b3b6a5208cb513f727564a84f1bb36ac04d4049
kwonmin123123 <zzacjk@naver.com>
2021-12-06 12:55:27 +0900

    Create Controller04.java

---
+++ src/main/java/org/zerock/controller/p01controller/Controller04.java
@@ -0,0 +1,55 @@
+package org.zerock.controller.p01controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.controller.p01controller.bean.Bean01;
+import org.zerock.controller.p01controller.bean.Bean02;
+
+@Controller
+@RequestMapping("/cont04")
+public class Controller04 {
+
+	@RequestMapping("/met01")
+	public void met1(String name, String city) {
+		
+		
+		System.out.println(name);
+		System.out.println(city);
+	}
+
+	
+	@RequestMapping("/met02")
+	public void method02(HttpServletRequest request) {
+		
+		// 2. request 분석/가공
+		String name = request.getParameter("name");
+		String city = request.getParameter("city");
+		
+		Bean01 bean = new Bean01();
+		bean.setName(name);
+		bean.setCity(city);
+		
+		System.out.println(bean); // 잘 들어왔는지 확인
+		
+		// 3. business logic
+		
+		// 4. add attributes
+		
+		// 5. forward / redirect
+	}
+	
+	@RequestMapping("/met03")
+	public void method03(Bean01 bean) {
+		
+		System.out.println(bean); // 잘 들어왔는지 확인
+	}
+	@RequestMapping("/met04")
+	public void met04(Bean02 bean) {
+		
+		System.out.println(bean);
+	}
+
+
+}
