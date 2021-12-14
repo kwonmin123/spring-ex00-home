5c0001a0701380179bc52c8f849e6404d76e0cd2
kwonmin123123 <zzacjk@naver.com>
2021-12-14 09:08:14 +0900

    Update BoardMapperTest.java

--- src/test/java/org/zerock/mapper/project01/BoardMapperTest.java
+++ src/test/java/org/zerock/mapper/project01/BoardMapperTest.java
@@ -10,7 +10,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.zerock.domain.project01.BoardVO;
+import org.zerock.domain.project1.BoardVO;
+import org.zerock.mapper.project1.BoardMapper;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 public class BoardMapperTest {
